f2fb12f04c0fd75e0716a1886214d8a3781baba8
Webster Fievre <108379057+WebsterFever@users.noreply.github.com>
2026-09-19 21:51:39 -0400

    feat: register products provider

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,7 @@ import App from './App.tsx'
 import { AuthProvider } from './contexts/AuthContext'
 import { CartProvider } from './contexts/CartContext'
 import { OrdersProvider } from './contexts/OrdersContext'
+import { ProductsProvider } from './contexts/ProductsContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -13,7 +14,9 @@ createRoot(document.getElementById('root')!).render(
       <AuthProvider>
         <CartProvider>
           <OrdersProvider>
-            <App />
+            <ProductsProvider>
+              <App />
+            </ProductsProvider>
           </OrdersProvider>
         </CartProvider>
       </AuthProvider>
