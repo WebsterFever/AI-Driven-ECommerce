7a46404975999395bccc56f84f921ff28484f895
Webster Fievre <108379057+WebsterFever@users.noreply.github.com>
2026-09-19 21:48:08 -0400

    feat: register orders provider

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import './index.css'
 import App from './App.tsx'
 import { AuthProvider } from './contexts/AuthContext'
 import { CartProvider } from './contexts/CartContext'
+import { OrdersProvider } from './contexts/OrdersContext'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
       <AuthProvider>
         <CartProvider>
-          <App />
+          <OrdersProvider>
+            <App />
+          </OrdersProvider>
         </CartProvider>
       </AuthProvider>
     </BrowserRouter>
